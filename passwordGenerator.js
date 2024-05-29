fe1c376b49cc747f701489ec6b32aa38972d6981
Sameer Kulkarni <88168602+sameerkulkarni27@users.noreply.github.com>
2024-05-28 19:13:47 -0700

    Update passwordGenerator.js

--- passwordGenerator.js
+++ passwordGenerator.js
@@ -1,14 +1,12 @@
-function myDate() {
-    document.getElementById("demo").innerHTML = "Lil Chungus";
-}
-
-function myStupid() {
-    document.getElementById("stupid").innerHTML = "dumbo";
-}
-
 function passwordGenerator() {
     var size = parseInt(document.getElementById("size").value);
     var result = "";
+
+    if (size <= 0) {
+        window.alert("Invalid: Enter a positive number for password size.");
+        return;
+    }
+
     for (let i = 0; i < size; i++) {
         var set = Math.floor(Math.random() * 4);
 
@@ -24,31 +22,27 @@ function passwordGenerator() {
         if (set ==  3) {
             result += getNumbers();
         }
+    }
 
-        function getNumbers() {
-            var length1 = Math.floor(Math.random() * 10) + 48;
-            // return parseChar(length1);
-            return String.fromCharCode(length1);
-        }
+    function getNumbers() {
+        var length1 = Math.floor(Math.random() * 10) + 48;
+        return String.fromCharCode(length1);
+    }
 
-        function getLETTERS() {
-            var length2 = Math.floor(Math.random() * 25) + 65;
-            //return parseChar(length2);
-            return String.fromCharCode(length2);
-        }
+    function getLETTERS() {
+        var length2 = Math.floor(Math.random() * 25) + 65;
+        return String.fromCharCode(length2);
+    }
 
-        function getLetters() {
-            var length3 = Math.floor(Math.random() * 25) + 97;
-            // return parseChar(length3);
-            return String.fromCharCode(length3);
-        }
+    function getLetters() {
+        var length3 = Math.floor(Math.random() * 25) + 97;
+        return String.fromCharCode(length3);
+    }
 
-        function getSymbols() {
-            var symbols = "!#$%&()*/?@_";
-            var length4 = Math.floor(Math.random() * 12) + 0;
-            // return parseChar(symbols.charAt(length4));
-            return symbols.charAt(length4);
-        }
+    function getSymbols() {
+        var symbols = "!#$%&()*/?@_";
+        var length4 = Math.floor(Math.random() * 12) + 0;
+        return symbols.charAt(length4);
     }
 
     document.getElementById("psadword").innerHTML = result;
